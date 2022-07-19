680f3a6198f5729c06443349742c47b683f7607f
uvineet53 <uvineet53@gmail.com>
2022-07-20 01:16:56 +0530

    shifted from fast api to flask

--- main.py
+++ main.py
@@ -1,38 +1,27 @@
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 import subprocess
-from pydantic import BaseModel
 import json
 import re
+from flask import Flask, request
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
 
 secrets = ['semgrep', '--config' ,'p/secrets']
 bsa = ['semgrep','--config','p/owasp-top-ten']
 full =['semgrep', '--config=auto']
 
-class Repo(BaseModel):
-    repo: str
-    rule: str
-    
-app = FastAPI()
-origins = ["*"]
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 pattern = "⋮┆----------------------------------------\\n\s\s\s\s\s[a-z]"
 
 def runScan(repo):
-    repository = repo.repo
+    repository = repo['repo']
     getRepo = subprocess.run(["git", "clone",repository.encode(),".test"],capture_output=True, text=True)
-    if(repo.rule == "secrets"):
+    if(repo['rule'] == "secrets"):
         result = subprocess.run(secrets, cwd='./.test',capture_output=True, text=True)
-    elif(repo.rule == "bsa"):
+    elif(repo['rule'] == "bsa"):
         result = subprocess.run(bsa, cwd='./.test',capture_output=True, text=True)
-    elif(repo.rule == "full"):
+    elif(repo['rule'] == "full"):
         result = subprocess.run(full, cwd='./.test',capture_output=True, text=True)
     subprocess.run(["rm","-rf",".test/"])
     data = result.stdout.split("Findings:")[1].strip().split("\n\n\n")
@@ -54,9 +43,14 @@ def runScan(repo):
     return finalData
 
 @app.get("/")
-async def root():
+def root():
     return {"message": "Welcome to Dunder Mifflin OSS Inspector"}
 
-@app.post("/scan/")
-async def run_scan(repo: Repo):
-    return {"results": runScan(repo)}
+@app.post("/scan")
+def run_scan():
+    repo = request.json
+    return {"results": runScan(repo=repo)}
+
+
+if __name__ == '__main__':
+    app.run()
--- renderResults.py
+++ renderResults.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-url = "http://127.0.0.1:8000/scan"
+url = "http://127.0.0.1:5000/scan"
 payload = {"repo":"https://github.com/0c34/govwa", "rule":'bsa'}
 res = requests.post(url,json=payload)
 data = json.loads(res.text)
