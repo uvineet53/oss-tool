66601e11e8d8799a5df233d5ac9ab17749d511b8
uvineet53 <uvineet53@gmail.com>
2022-07-28 16:18:32 +0530

    updated rules

--- constants.py
+++ constants.py
@@ -1,6 +1,15 @@
 rules = {
     "secrets": ["semgrep", "--config", "p/secrets"],
     "bsa": ["semgrep", "--config", "p/owasp-top-ten"],
+    "sql": ["semgrep", "--config", "p/sql-injection"],
+    "jwt": ["semgrep", "--config", "p/jwt"],
+    "r2c": ["semgrep", "--config", "p/r2c"],
+    "nodejs": ["semgrep", "--config", "p/nodejsscan"],
+    "react": ["semgrep", "--config", "p/react"],
+    "golang": ["semgrep", "--config", "p/golang"],
+    "javascript": ["semgrep", "--config", "p/javascript"],
+    "kotlin": ["semgrep", "--config", "p/kotlin"],
+    "django": ["semgrep", "--config", "p/django"],
     "full": ["semgrep", "--config=auto"],
 }
 
